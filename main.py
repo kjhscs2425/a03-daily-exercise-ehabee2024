35008fb8d2ba26f84db2d9257ab90ff83910b6fc
ehabee <ebender2025@kehillahstudent.org>
2024-09-26 11:32:17 -0700

    done

--- main.py
+++ main.py
@@ -2,54 +2,47 @@
 
 import turtle
 
-# `dance` is a ...[function/variable/parameter]...
+#`dance` is a [function]
 def dance():
   turtle.left(1000)
   turtle.right(500)
 
-# `pi` is a  ...[function/variable/parameter]...
+ #`pi` is a  ...variable.
 pi = 3.1415926535
 
-# `convert` is a ...[function/variable/parameter]...
-# and `celcius` is a ...[function/variable/parameter]...
+convert` is a function...
+`celcius` is a parameter]...
 def convert(celcius):
-  # `fahrenheit` is a  ...[function/variable/parameter]...
+ `fahrenheit` is a variable
   fahrenheit = celcius * 9 / 5 + 32
   print(fahrenheit)
 
-# `draw_spiral` is a ...[function/variable/parameter]...
-# and `num_loops` is a ...[function/variable/parameter]...
-# and `distance` is a ...[function/variable/parameter]...
-# and `angle` is a ...[function/variable/parameter]...
+#`draw_spiral` is a function
+#`num_loops` is a parameter
+#and `distance` is a parameter]...
+# and `angle` is a variable
 def draw_spiral(num_loops, distance, angle):
   for i in range(num_loops):
     for _ in range(2):
-      # `arm_length` is a ...[function/variable/parameter]...
+      `arm_length` is a variable
       arm_length = i*distance
       turtle.forward(arm_length)
       turtle.left(angle)
 
-# `say_hello` is a ...[function/variable/parameter]...
-# and `name` is a ...[function/variable/parameter]...
-def say_hello(name):
+# say_hello` is a function
+`name` is a function
+# say_hello(name):
   print("Hello, " + name + "!")
   print("How are you today?")
 
-# 2. Call `say_hello`
-#    a) Call `say_hello` with the argument "Dr. EB"
-
-#    b) Call `say_hello` with your name as the argument
-
-#    c) Call `say_hello` with a friend's name as the argument
-
-#     a) Call 'say hello' (Dr EB)
-#      import turtle
-#      open turtle
-#    Move Turtle (90)
-#     Move Turtle (40)
-#    Move Turtle (50
-    turtle.exitonclick
-
+ #2. Call `say_hello`
+ # a) Call `say_hello` with the argument "Dr. EB"
+say_hello("Dr. EB")
+ # b) Call `say_hello` with your name as the argument
+say_hello("Eric")
+ c) Call `say_hello` with a friend's name as the argument
+say_hello("Graham")
+   a) Call 'say hello' (Dr EB)
 
 
 # 3. Call `draw_spiral` with the following arguments:
@@ -65,4 +58,4 @@ def say_hello(name):
 #       use 1 for `distance`
 #       use 60 for `angle`
 
-turtle.exitonclick()
+# turtle.exitonclick()
